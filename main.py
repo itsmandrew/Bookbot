692021b6ae2fb63c50c3611f39206c08044286cc
itsmandrew <theachang1688@gmail.com>
2024-05-08 19:52:54 -0700

    Added generate report

--- main.py
+++ main.py
@@ -9,10 +9,12 @@ def main():
     with open("books/frankenstein.txt", encoding="utf-8") as file_book:
         file_contents = file_book.read()
 
-    print(file_contents)
+    number_of_words = count_words(file_contents)
+    letters_map = count_letters(file_contents)
 
+    res = generate_report(number_of_words, letters_map)
 
-
+    print(res)
 
 def count_words(text):
     """
@@ -25,8 +27,6 @@ def count_words(text):
         int: The number of words in the given text file.
     """
     words = text.split()
-
-    print(len(words))
 
     return len(words)
 
@@ -46,9 +46,26 @@ def count_letters(text):
     for word in words:
         for letter in word:
             hash_map[letter] += 1
-    print(hash_map)
     return hash_map
 
+def generate_report(word_count, letter_map):
+    """
+    Generates a report of the total number of words and prints out the number 
+    of occurences of each character.
+
+    Args:
+        word_count (int): number of words in the file
+        letter_map (defaultdict): hash-map of the -> letters : occurences
+
+    Returns:
+        string: the report
+    """
+
+    res = "--- Begin report of books/frankenstein.txt ---\n"
+    res += f"{word_count} words found in the document\n"
+
+
+    return res
 
 if __name__ == "__main__":
     main()
